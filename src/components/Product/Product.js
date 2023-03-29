8b54f46e1ed04f17f925679311e6c2dcf9e29c40
RahmanAdel <rahmanadel619@gmail.com>
2023-03-29 16:48:51 +0600

    added cart component and calculation

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 const Product = (props) => {
 
   const { img, name, seller, ratings, price } = props.product;
+  const handleAddToCart = props.handleAddToCart;
 
   return (
     <div className='product'>
@@ -13,7 +16,10 @@ const Product = (props) => {
         <p>Manufacturer: {seller}</p>
         <p>Rating: {ratings} stars</p>
       </div>
-      <button className='btn-cart'>Add To Cart</button>
+      <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>
+        Add To Cart
+        <FontAwesomeIcon icon={faShoppingCart} />
+      </button>
     </div>
   );
 };
